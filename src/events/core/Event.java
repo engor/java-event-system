1b2bc5799585ed6ee9ad5449d4e5681ef7e6683f
Evgeniy Goroshkin <engor@yandex.ru>
2015-11-06 22:20:24 +0700

    add lock / unlock and delayed list. for what? in previous revision - if you unsubscribe during sendEvent method, you may take error - changing collection inside foreach.

--- src/events/core/Event.java
+++ src/events/core/Event.java
@@ -10,32 +10,63 @@ import java.util.List;
 public class Event<TArgs extends EventArgs> implements IEventSender<TArgs> {
 
     private List<IEventListener<TArgs>> listeners;
+    private List<Runnable> delayedCommands;
+    private boolean isLocked;
     
     @Override
     public void sendEvent(Object sender, TArgs args) {
         if (listeners == null) return;
+        lock();
         for (IEventListener<TArgs> i : listeners) {
             i.onReceived(sender, args);
         }
+        unlock();
     }
     
-    public void subscribe(IEventListener<TArgs> listener) {
+    public void subscribe(final IEventListener<TArgs> listener) {
         if (listener == null) return;
         if (listeners == null) {
             listeners = new ArrayList<>();
         }
-        listeners.add(listener);
+        if (!isLocked) {
+            listeners.add(listener);
+        } else {
+            addDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    listeners.add(listener);
+                }
+            });
+        }
     }
 
-    public void unsubscribe(IEventListener<TArgs> listener) {
+    public void unsubscribe(final IEventListener<TArgs> listener) {
         if (listener == null) return;
         if (listeners == null) return;
-        listeners.remove(listener);
+        if (!isLocked) {
+            listeners.remove(listener);
+        } else {
+            addDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    listeners.remove(listener);
+                }
+            });
+        }
     }
 
     public void unsubscribeAll() {
         if (listeners == null) return;
-        listeners.clear();
+        if (!isLocked) {
+            listeners.clear();
+        } else {
+            addDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    listeners.clear();
+                }
+            });
+        }
     }
 
     public boolean hasListeners() {
@@ -44,6 +75,31 @@ public class Event<TArgs extends EventArgs> implements IEventSender<TArgs> {
 
     public boolean canBeReceived() {
         return listeners != null && !listeners.isEmpty();
+    }
+
+    private void lock() {
+        isLocked = true;
+    }
+    private void unlock() {
+        isLocked = false;
+        if (delayedCommands == null) return;
+        synchronized (delayedCommands) {
+            if (!delayedCommands.isEmpty()) {
+                for (Runnable r :
+                        delayedCommands) {
+                    r.run();
+                }
+                delayedCommands.clear();
+            }
+        }
+    }
+
+    private void addDelayed(Runnable r) {
+        if (delayedCommands == null)
+            delayedCommands = new ArrayList<>();
+        synchronized (delayedCommands) {
+            delayedCommands.add(r);
+        }
     }
 
 }
